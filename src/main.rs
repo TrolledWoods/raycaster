67038f024714b9e74f6009c5cf93889593a0bc8d
John Hörnvall <trolledwoods@gmail.com>
2020-10-04 20:45:33 +0200

    Added framerate counter

--- src/main.rs
+++ src/main.rs
@@ -38,14 +38,18 @@ fn main() {
     });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(std::time::Duration::from_secs_f32(1.0 / 60.0)));
 
 	let mut player_x = 5.0;
 	let mut player_y = 5.0;
 
 	let mut player_rot: f32 = 0.0;
 
+	let mut frame_rate = [0f32; 10];
+	let mut frame_rate_index = 0;
     while window.is_open() && !window.is_key_down(Key::F4) {
+		let instant = std::time::Instant::now();
+
 		if window.is_key_down(Key::Right) {
 			player_rot -= 0.1;
 		}
@@ -100,5 +104,13 @@ fn main() {
         window
             .update_with_buffer(&buffer, WIDTH, HEIGHT)
             .unwrap();
+
+		frame_rate[frame_rate_index] = instant.elapsed().as_secs_f32();
+		frame_rate_index += 1;
+		if frame_rate_index >= frame_rate.len() {
+			frame_rate_index = 0;
+			let average: f32 = frame_rate.iter().sum::<f32>() / frame_rate.len() as f32;
+			println!("{} seconds / {} fps", average, 1.0 / average);
+		}
     }
 }
